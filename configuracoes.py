2085d4a5499f83ac79cffc2a335328a435ef4b06
Zhenriquee <83546851+Zhenriquee@users.noreply.github.com>
2025-12-29 17:07:14 +0000

    modified:   __pycache__/configuracoes.cpython-312.pyc 	modified:   configuracoes.py

--- configuracoes.py
+++ configuracoes.py
@@ -1,10 +1,13 @@
 import os
+from pathlib import Path
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DB_FOLDER = os.path.join(BASE_DIR, 'data')
 DB_NAME = 'dados_ans_paralela.db'
-DATABASE_PATH = '/workspaces/ANALISE_OPERADORAS/data/base_ans_paralela.db'#os.path.join(DB_FOLDER, DB_NAME)
+base_dire = Path(__file__).resolve().parents[2]
+
+DATABASE_PATH = base_dire / 'queries' / 'filtros' / 'listar_todas_operadoras.sql' #'/workspaces/ANALISE_OPERADORAS/data/base_ans_paralela.db'#os.path.join(DB_FOLDER, DB_NAME)
 
 QUERIES_PATH = os.path.join(BASE_DIR, 'queries')
 
